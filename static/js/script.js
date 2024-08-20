7f515377670ffac7471329ab2693ee93d14868b3
Dominik <dominikpiekarek1@gmail.com>
2024-08-21 00:50:36 +0200

    added collapsed

--- static/js/script.js
+++ static/js/script.js
@@ -18,6 +18,15 @@ function addParentNode(parent) {
   const newUl = document.createElement("ul");
   const newLi = document.createElement("li");
 
+  const toggleButton = document.createElement("button");
+  toggleButton.classList.add("toggle-button", "collapsed");
+  toggleButton.addEventListener("click", (event) => {
+    toggleButton.classList.toggle("collapsed");
+    Array(event.target.parentNode.getElementsByTagName("ul")).forEach((item) =>
+      [...item].map((element) => element.classList.toggle("hide"))
+    );
+  });
+
   const addNewNode = document.createElement("button");
   addNewNode.textContent = "Add Child Node";
   addNewNode.addEventListener("click", () => addParentNode(newLi));
@@ -26,6 +35,7 @@ function addParentNode(parent) {
   deleteNode.textContent = "Delete Child Node";
   deleteNode.addEventListener("click", () => newLi.remove());
 
+  newLi.appendChild(toggleButton);
   newLi.appendChild(addNewNode);
   newLi.appendChild(deleteNode);
   newUl.appendChild(newLi);
